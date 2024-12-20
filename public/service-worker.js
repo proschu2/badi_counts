3b0e71c346d598cfe77a5cee3d7495009ef526d6
proschu2 <github@sanziomonti.xyz>
2024-12-20 10:14:39 +0100

    add icons

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "hall-bad-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  // ...add other assets like CSS, JS, and images if needed
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
